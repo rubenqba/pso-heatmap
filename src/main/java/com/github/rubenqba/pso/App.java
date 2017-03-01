c0a7ec82f770a6141dbfbd36bbe47d49cbb074c6
Rubén Bresler Camps <ruben1981@gmail.com>
2017-03-01 08:49:02 -0600

    Initial commit

---
+++ src/main/java/com/github/rubenqba/pso/App.java
@@ -0,0 +1,13 @@
+package com.github.rubenqba.pso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
